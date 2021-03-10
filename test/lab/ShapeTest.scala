2eebc15cd0471067370f5941b8edac37a3bad5bf
mattiaachilli <mattia.achilli@studio.unibo.it>
2021-03-10 13:19:10 +0100

    red-green-refactor testArea

--- test/lab/ShapeTest.scala
+++ test/lab/ShapeTest.scala
@@ -2,18 +2,25 @@ package lab
 
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test;
+import Shape.Shape._
 
 class ShapeTest {
 
   import lab.Shape._;
 
-  val square: Shape.Square = Shape.Square(5)
-  val rectangle: Shape.Rectangle = Shape.Rectangle(6, 4)
-  val circle: Shape.Circle = Shape.Circle(5)
+  val square: Square = Square(5)
+  val rectangle: Rectangle = Rectangle(6, 4)
+  val circle: Circle = Circle(5)
 
   @Test def testPerimeter(): Unit = {
-    assertEquals(20, Shape.perimeter(square))
-    assertEquals(20, Shape.perimeter(rectangle))
-    assertEquals(2 * 5 * Math.PI, Shape.perimeter(circle))
+    assertEquals(20, perimeter(square))
+    assertEquals(20, perimeter(rectangle))
+    assertEquals(2 * 5 * Math.PI, perimeter(circle))
+  }
+
+  @Test def testArea(): Unit = {
+    assertEquals(25, area(square))
+    assertEquals(24, area(rectangle))
+    assertEquals(Math.PI * Math.pow(5, 2), area(circle))
   }
 }
